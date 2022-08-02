689a83c26186bc8ab60aeae27fff978acce2b289
Mathad3 <109080225+Mathad3@users.noreply.github.com>
2022-08-02 08:11:11 -0700

    l

---
+++ comparision.py
@@ -0,0 +1,17 @@
+from operator import truediv
+
+
+a=13
+b=55
+a>b is False
+print(a>b)
+a<b is True
+print(a<b)
+a==b is False
+print(a==b)
+a!=b is True
+a>=b is False
+print(a>=b)
+a<=b is True
+print(a<=b)
+ 
