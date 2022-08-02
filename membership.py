5fb14a34906ae90ab6e9e01a8edd7a6f556a5505
Mathad3 <109080225+Mathad3@users.noreply.github.com>
2022-08-02 09:14:32 -0700

    .

---
+++ membership.py
@@ -0,0 +1,11 @@
+x=42
+y=19
+list=[10,20,30,40,50]
+if ("x is not in the list"):
+    print("x is not present in the given list")
+else:
+    print("x is present in the given list")
+if ("y in list"):
+    print("y is present in the given list")
+else:
+    print("y is not present in the given list")
